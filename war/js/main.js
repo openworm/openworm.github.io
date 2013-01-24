a0c7f22f8eb2b865772beac72aa0daf27c5c60c0
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2013-01-24 16:04:33 +0000

    Updated bootstrap added scaffholding for get_involved

--- war/js/main.js
+++ war/js/main.js
@@ -1,6 +1,7 @@
-
 $(function()
 {
+	var $window = $(window)
+
 	$("#news-feed").PaRSS("http://openworm.tumblr.com/rss", // url to the feed
 	4, // number of items to retrieve
 	"M jS Y, g:i a", // date format
@@ -9,4 +10,29 @@ $(function()
 	{/* optional callback function performed after list is appended to the page */
 	});
 
+	// side bar
+	$('.bs-docs-sidenav').affix(
+	{
+		offset :
+		{
+			top : function()
+			{
+				return $window.width() <= 980 ? 290 : 210
+			},
+			bottom : 270
+		}
+	})
+
+
+	$('.nav li').click(function(e)
+	{
+		$('.nav li').removeClass('active');
+		
+		var $this = $(this);
+		if (!$this.hasClass('active'))
+		{
+			$this.addClass('active');
+		}
+		
+	});
 });
