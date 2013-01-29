619ccf25f9744ea6e17dde7bd70d1ba4c2730b42
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2013-01-29 13:13:28 +0000

    Various changes

--- war/js/main.js
+++ war/js/main.js
@@ -38,3 +38,12 @@ $(function()
 	
 	$('.minilogo').tooltip();
 });
+
+window.___gcfg = {lang: 'en-GB'};
+
+(function() {
+  var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+  po.src = 'https://apis.google.com/js/plusone.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+})();
+
