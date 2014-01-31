adb20effd0bba26ba4671e2524e7808baa09b4f3
johnidol <giovanni.idili@gmail.com>
2014-01-31 19:06:38 +0000

    added days countdown

--- war/js/main.js
+++ war/js/main.js
@@ -56,3 +56,23 @@ window.___gcfg = {lang: 'en-GB'};
   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
 })();
 
+// countdown time to kickstarter
+function showRemainingDays(end) {
+	var _second = 1000;
+	var _minute = _second * 60;
+	var _hour = _minute * 60;
+	var _day = _hour * 24;
+	
+    var now = new Date();
+    var distance = end - now;
+    if (distance < 0) {
+        document.getElementById('countdown').innerHTML = 'EXPIRED!';
+        return;
+    }
+    var days = Math.floor(distance / _day);
+    var hours = Math.floor((distance % _day) / _hour);
+    var minutes = Math.floor((distance % _hour) / _minute);
+    var seconds = Math.floor((distance % _minute) / _second);
+
+    document.getElementById('countdown').innerHTML = days + ' days';
+}
