12750ce9493fdcee0af790973cdddfb0e3d01b1c
johnidol <giovanni.idili@gmail.com>
2016-10-17 12:02:57 +0100

    some formatting

--- war/js/repositories.js
+++ war/js/repositories.js
@@ -5,9 +5,9 @@ var urls = [
     "https://cdn.rawgit.com/openworm/OpenWorm/master/.openworm.yml",
     "https://cdn.rawgit.com/openworm/hodgkin_huxley_tutorial/master/.openworm.yml",
     "https://cdn.rawgit.com/openworm/openworm_docs/master/.openworm.yml",
-    //"https://cdn.rawgit.com/openworm/simple-C-elegans/master/.openworm.yml",
+    // "https://cdn.rawgit.com/openworm/simple-C-elegans/master/.openworm.yml",
     "https://cdn.rawgit.com/openworm/wormbrowser/master/.openworm.yml",
-    //    "https://cdn.rawgit.com/openworm/sibernetic/master/.openworm.yml",
+    // "https://cdn.rawgit.com/openworm/sibernetic/master/.openworm.yml",
     "https://cdn.rawgit.com/openworm/openwormbrowser-ios/master/.openworm.yml",
     "https://cdn.rawgit.com/openworm/CElegansNeuroML/master/.openworm.yml",
     "https://cdn.rawgit.com/openworm/org.openworm.website/master/.openworm.yml",
@@ -29,12 +29,11 @@ var domGroup = "domGroup";
 var tabGroup = "tabGroup";
 
 // *CWL* establishes relationship between repo keys and indices.
-//   Indices are used as part of DOM ids.
+// Indices are used as part of DOM ids.
 var groupLookup = {};
 var elementLookup = {};
 
 var fetch = function(container, urls, index) {
-
     $.ajax({
         type: 'GET',
         dataType: 'text',
@@ -42,86 +41,96 @@ var fetch = function(container, urls, index) {
         success: function(responseData, textStatus, jqXHR) {
             var nativeObject = YAML.parse(responseData);
 
-	    var hasParent = false;
-	    // *CWL* Prior to this, I had a *profound* lack of understanding
-	    //   for how hierarchy was handled.
-	    //
-	    // Also there is no longer any need to worry about navigation
-	    //   issues with basic tabs. The question now is how we can
-	    //   use buttons to work as if a tab had been clicked.
-	    var inner;
-	    inner = $('<div id="' + domElement + index + '" ></div>');
-	    
-	    // **CWL** Kind of a hack for now. Establish a string->meta lookuptable.
-	    elementLookup[nativeObject.repo] = index;
+            var hasParent = false;
+            // *CWL* Prior to this, I had a *profound* lack of
+            // understanding
+            // for how hierarchy was handled.
+            //
+            // Also there is no longer any need to worry about
+            // navigation
+            // issues with basic tabs. The question now is how we can
+            // use buttons to work as if a tab had been clicked.
+            var inner;
+            inner = $('<div id="' + domElement + index + '" ></div>');
 
-	    // **CWL** and this is the solution for hiding everything at first.
-	    //   Initially I had tried to hide the container before going into this
-	    //   loop but that didn't work out very well.
-	    //
-	    // On discovering how native Bootstrap tab navigation works, all the
-	    //   shaneniggans with manually messing with hidden properties go away! 
-	    //	    inner.hide();
-	    container.append(inner);
+            // **CWL** Kind of a hack for now. Establish a string->meta
+            // lookuptable.
+            elementLookup[nativeObject.repo] = index;
+
+            // **CWL** and this is the solution for hiding everything at
+            // first.
+            // Initially I had tried to hide the container before going
+            // into this
+            // loop but that didn't work out very well.
+            //
+            // On discovering how native Bootstrap tab navigation works,
+            // all the
+            // shaneniggans with manually messing with hidden properties
+            // go away!
+            // inner.hide();
+            container.append(inner);
 
             inner.append('<p><b>Repo:</b> ' + nativeObject.repo + '</p>');
             inner.append('<p><b>Short Description:</b> ' + nativeObject.shortDescription + '</p>');
-	    if (nativeObject.documentation != undefined) {
-		inner.append('<p><b>Documentation:</b> <a href=\"' + 
-				 nativeObject.documentation +
-				 '\" target=\"blank\">' +
-				 nativeObject.documentation +
-				 '</a></p>');
-	    } else {
-		inner.append('<p><b>Documentation:</b> None</p>');
-	    }
+            if (nativeObject.documentation != undefined) {
+                inner.append('<p><b>Documentation:</b> <a href=\"' + nativeObject.documentation + '\" target=\"blank\">' + nativeObject.documentation + '</a></p>');
+            } else {
+                inner.append('<p><b>Documentation:</b> None</p>');
+            }
 
-            inner.append('<p><b>Coordinator:</b> ' + nativeObject.coordinator + '</p>');
+            inner.append('<p><b>Coordinator:</b> ' +
+                nativeObject.coordinator + '</p>');
 
             if (nativeObject.parent != undefined) {
-            	inner.append('<p><b>Parent:</b> <a class="btn btn-link" href="' + repo_url + nativeObject.parent[0] + '">Visit Repo</a> <a class="btn btn-primary btn-xs navBtn" href="#">' + nativeObject.parent[0] + '</a></p>');
-		add_to_hierarchy(nativeObject.repo,nativeObject.parent);
-		hasParent = true;
+                inner.append('<p><b>Parent:</b> <a class="btn btn-link" href="' + repo_url + nativeObject.parent[0] + '">Visit Repo</a> <a class="btn btn-primary btn-xs navBtn" href="#">' + nativeObject.parent[0] + '</a></p>');
+                add_to_hierarchy(nativeObject.repo, nativeObject.parent);
+                hasParent = true;
             }
 
             if (nativeObject.inputs != undefined) {
-            	inner.append('<p><b>Inputs:</b></p>');
+                inner.append('<p><b>Inputs:</b></p>');
                 for (var i = 0; i < nativeObject.inputs.length; i++) {
-		    inner.append('<p><a class="btn btn-link" href="' + repo_url + nativeObject.inputs[i] + '">Visit Repo</a> <a class="btn btn-primary btn-xs navBtn" href="#">' + nativeObject.inputs[i] + '</a></p>');
+                    inner.append('<p><a class="btn btn-link" href="' + repo_url + nativeObject.inputs[i] + '">Visit Repo</a> <a class="btn btn-primary btn-xs navBtn" href="#">' + nativeObject.inputs[i] + '</a></p>');
                 }
             }
-            
+
             if (nativeObject.outputs != undefined) {
-            	inner.append('<p><b>Outputs:</b></p>');
+                inner.append('<p><b>Outputs:</b></p>');
                 for (var i = 0; i < nativeObject.outputs.length; i++) {
-		    inner.append('<p><a class="btn btn-link" href="' + repo_url + nativeObject.outputs[i] + '">Visit Repo</a> <a class="btn btn-primary btn-xs navBtn" href="#">' + nativeObject.outputs[i] + '</a></p>');
+                    inner.append('<p><a class="btn btn-link" href="' +
+                        repo_url +
+                        nativeObject.outputs[i] +
+                        '">Visit Repo</a> <a class="btn btn-primary btn-xs navBtn" href="#">' +
+                        nativeObject.outputs[i] +
+                        '</a></p>');
                 }
             }
 
             if (nativeObject.children != undefined) {
-            	inner.append('<p><b>Children:</b></p>');
+                inner.append('<p><b>Children:</b></p>');
                 for (var i = 0; i < nativeObject.children.length; i++) {
-		    inner.append('<p><a class="btn btn-link" href="' + repo_url + nativeObject.children[i] + '">Visit Repo</a> <a class="btn btn-primary btn-xs navBtn" href="#">' + nativeObject.children[i] + '</a></p>');
-		    add_to_hierarchy(nativeObject.children[i],nativeObject.repo);
+                    inner.append('<p><a class="btn btn-link" href="' + repo_url + nativeObject.children[i] + '">Visit Repo</a> <a class="btn btn-primary btn-xs navBtn" href="#">' + nativeObject.children[i] + '</a></p>');
+                    add_to_hierarchy(nativeObject.children[i],nativeObject.repo);
                 }
             }
 
-	    if (!hasParent) {
-		add_to_root(nativeObject.repo);
-	    }
+            if (!hasParent) {
+                add_to_root(nativeObject.repo);
+            }
 
-	    inner.append('<hr style="height: 1px; border-color: black;">');
+            inner.append('<hr style="height: 1px; border-color: black;">');
 
             // fetch next
             if (urls.length - 1 > index) {
                 fetch(container, urls, index + 1);
             } else {
-		build_hierarchy();
-		disable_untracked_nav_buttons();
-		// **CWL** Generate tabs from newly acquired data
-		// handle asynchrony issues by making sure all elements are populated.
-		cwlpager_init(container);
-	    }
+                build_hierarchy();
+                disable_untracked_nav_buttons();
+                // **CWL** Generate tabs from newly acquired data
+                // handle asynchrony issues by making sure all elements
+                // are populated.
+                cwlpager_init(container);
+            }
         },
         error: function(responseData, textStatus, errorThrown) {
             alert('GET failed.');
