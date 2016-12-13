7dae638f673ad910fa47b24e8189cf1d0246519b
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2016-12-13 18:34:10 +0000

    Improved donations

--- war/js/main.js
+++ war/js/main.js
@@ -41,7 +41,69 @@ $(function() {
     });
 
     $('.minilogo').tooltip();
+    
+    $(".donation").click(function(){
+    	$(".donation").removeClass("active");
+    	$("#otherAmount").removeClass("active");
+    	$("#amountSent").attr("value",$(this).html().replace("$",""));
+    	$(this).addClass("active");
+    });
+    
+    $(".other").click(function(){
+    	$("#otherAmount").addClass("active");
+    	$("#amountSent").attr("value",$(this).val());
+    	$("#otherAmount").focus();
+    });
+    
+    $("#otherAmount").click(function(){
+    	$("#otherAmount").addClass("active");
+    	$(".donation").removeClass("active");
+    	$("#amountSent").attr("value",$(this).val());
+    	$(".other").addClass("active");
+    });
+    
+    $("#otherAmount").on("input",function(){
+    	$("#amountSent").attr("value",$(this).val());
+    });
+
+    var amount = getUrlParameter('amount');
+    if(amount=="" || amount==undefined){
+    	$("#d50").click();
+    }
+    else if(amount=="5"){
+    	$("#d5").click();
+    }
+    else if(amount=="25"){
+    	$("#d25").click();
+    }
+	else if(amount=="50"){
+		$("#d50").click();
+	}
+	else if(amount=="100"){
+		$("#d100").click();	
+	}
+	else{
+		$(".other").click();
+		$("#otherAmount").val(amount);
+	}
+
 });
+
+
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
 
 window.___gcfg = {
     lang: 'en-GB'
@@ -55,6 +117,17 @@ window.___gcfg = {
     var s = document.getElementsByTagName('script')[0];
     s.parentNode.insertBefore(po, s);
 })();
+
+function donate(){
+    var amount = $(".donation.active");
+    if(amount.hasClass("other")){
+    	amount=$("#otherAmount").val();
+    }
+    else{
+    	amount=amount.html().replace("$","");
+    }
+    window.location="\donate.html?amount="+amount;
+}
 
 // countdown time to kickstarter
 function showRemainingDays(end) {
