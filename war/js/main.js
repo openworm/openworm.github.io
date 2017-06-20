623f7c9b03a68c1eb7ea811c14602d4bad21b64a
Sam Beck <thesamuelbeck@gmail.com>
2017-06-19 23:57:44 -0700

    working templated site with jquery only. uses dumb (but correct) code to set active <li> from nav bar

--- war/js/main.js
+++ war/js/main.js
@@ -1,94 +1,92 @@
-$(function() {
-    var $window = $(window)
+var $window = $(window)
 
-    $("#news-feed").PaRSS("http://openworm.tumblr.com/rss", // url to the feed
-        6, // number of items to retrieve
-        "M jS Y, g:i a", // date format
-        false, // include descriptions
-        function() {
-            /*
-             * optional callback function performed after list is appended to the
-             * page
-             */
-        });
+$("#news-feed").PaRSS("http://openworm.tumblr.com/rss", // url to the feed
+		      6, // number of items to retrieve
+		      "M jS Y, g:i a", // date format
+		      false, // include descriptions
+		      function() {
+			  /*
+			   * optional callback function performed after list is appended to the
+			   * page
+			   */
+		      });
 
-    // side bar
-    $('.bs-docs-sidenav').affix({
-        offset: {
-            top: function() {
-                return $window.width() <= 980 ? 290 : 210
-            },
-            bottom: 270
-        }
-    })
-
-    $(".carousel-control").click(function(e) {
-        $("#tip").hide();
-    });
-
-    $('.carousel').carousel({
-        interval: 13000
-    });
-
-    $('.nav li').click(function(e) {
-        $('.nav li').removeClass('active');
-
-        var $this = $(this);
-        if (!$this.hasClass('active')) {
-            $this.addClass('active');
-        }
-
-    });
-
-    $('.minilogo').tooltip();
-    
-    $(".donation").click(function(){
-    	$(".donation").removeClass("active");
-    	$("#otherAmount").removeClass("active");
-    	$("#amountSent").attr("value",$(this).html().replace("$",""));
-    	$(this).addClass("active");
-    });
-    
-    $(".other").click(function(){
-    	$("#otherAmount").addClass("active");
-    	$("#amountSent").attr("value",$(this).val());
-    	$("#otherAmount").focus();
-    });
-    
-    $("#otherAmount").click(function(){
-    	$("#otherAmount").addClass("active");
-    	$(".donation").removeClass("active");
-    	$("#amountSent").attr("value",$(this).val());
-    	$(".other").addClass("active");
-    });
-    
-    $("#otherAmount").on("input",function(){
-    	$("#amountSent").attr("value",$(this).val());
-    });
-
-    var amount = getUrlParameter('amount');
-    if(amount=="" || amount==undefined){
-    	$("#d50").click();
+// side bar
+$('.bs-docs-sidenav').affix({
+    offset: {
+        top: function() {
+            return $window.width() <= 980 ? 290 : 210
+        },
+        bottom: 270
     }
-    else if(amount=="5"){
-    	$("#d5").click();
+})
+
+$(".carousel-control").click(function(e) {
+    $("#tip").hide();
+});
+
+$('.carousel').carousel({
+    interval: 13000
+});
+
+$('.nav li').click(function(e) {
+    $('.nav li').removeClass('active');
+
+    var $this = $(this);
+    if (!$this.hasClass('active')) {
+        $this.addClass('active');
     }
-    else if(amount=="25"){
-    	$("#d25").click();
-    }
-	else if(amount=="50"){
-		$("#d50").click();
-	}
-	else if(amount=="100"){
-		$("#d100").click();	
-	}
-	else{
-		$(".other").click();
-		$("#otherAmount").val(amount);
-		$("#amountSent").attr("value",amount);
-	}
 
 });
+
+$('.minilogo').tooltip();
+
+$(".donation").click(function(){
+    $(".donation").removeClass("active");
+    $("#otherAmount").removeClass("active");
+    $("#amountSent").attr("value",$(this).html().replace("$",""));
+    $(this).addClass("active");
+});
+
+$(".other").click(function(){
+    $("#otherAmount").addClass("active");
+    $("#amountSent").attr("value",$(this).val());
+    $("#otherAmount").focus();
+});
+
+$("#otherAmount").click(function(){
+    $("#otherAmount").addClass("active");
+    $(".donation").removeClass("active");
+    $("#amountSent").attr("value",$(this).val());
+    $(".other").addClass("active");
+});
+
+$("#otherAmount").on("input",function(){
+    $("#amountSent").attr("value",$(this).val());
+});
+
+var amount = getUrlParameter('amount');
+if(amount=="" || amount==undefined){
+    $("#d50").click();
+}
+else if(amount=="5"){
+    $("#d5").click();
+}
+else if(amount=="25"){
+    $("#d25").click();
+}
+else if(amount=="50"){
+    $("#d50").click();
+}
+else if(amount=="100"){
+    $("#d100").click();
+}
+else{
+    $(".other").click();
+    $("#otherAmount").val(amount);
+    $("#amountSent").attr("value",amount);
+}
+
 
 
 var getUrlParameter = function getUrlParameter(sParam) {
